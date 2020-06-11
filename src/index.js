03f5f2c64485a02b5ba7cc0ef483141ce74a8c97
Muhammad Khizer Kamal <64361892+khizerkamal@users.noreply.github.com>
2020-06-11 20:27:09 +0500

    Update AddTransaction.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createRequire } from 'module';
 
+const require = createRequire(import.meta.url);
 
 ReactDOM.render(
   <React.StrictMode>
